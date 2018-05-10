d2809a85c5777ed4613bda323142482be566fa61
Mondo <568197478@qq.com>
2018-05-10 14:38:48 +0800

    文件备份

---
+++ sublime-setting.js
@@ -0,0 +1,11 @@
+{
+	"always_show_minimap_viewport" : true,
+	"bold_folder_labels"           : true,
+	"font_options"                 : ["gray_antialias", "subpixel_antialias"], // On retina Mac & Windows
+	"indent_guide_options"         : ["draw_normal", "draw_active"], // Highlight active indent
+	"line_padding_bottom"          : 3,
+	"line_padding_top"             : 3,
+	"overlay_scroll_bars"          : "enabled",
+	"font_size": 13,
+	"tab_size": 2
+}
